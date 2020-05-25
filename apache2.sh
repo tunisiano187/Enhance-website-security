b6ad7a80e51b1d7e98f2787c2e8b7b5bf82c3f1a
tunisiano187 <fabian@bowlman.org>
2020-05-25 09:01:17 +0200

    First commit

---
+++ apache2.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+sed -i "s/-SSLv3$/-SSLv3 -TLSv1 -TLSv1.1/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:ECDHE-RSA-AES128-SHA256:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:ECDHE-RSA-AES256-SHA384:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:ECDHE-RSA-AES128-SHA:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:ECDHE-RSA-AES256-SHA:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:DHE-RSA-AES128-SHA256:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:DHE-RSA-AES128-SHA:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:DHE-RSA-AES256-SHA256:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:DHE-RSA-AES256-SHA:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:AES128-GCM-SHA256:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:AES256-GCM-SHA384:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:AES128-SHA256:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:AES256-SHA256:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:AES128-SHA:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:AES256-SHA:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+service apache2 restart
