3cec6c9e1a3b1864a4fdf6d19d351972d4f0a6e6
Tunisiano18 <tunisiano187@users.noreply.github.com>
2023-07-10 16:13:58 +0200

    Update apache2.sh
    DHE-RSA-AES256-GCM-SHA256
    DHE-RSA-AES256-GCM-SHA384

--- apache2.sh
+++ apache2.sh
@@ -14,4 +14,6 @@ sed -i "s/:AES128-SHA256:/:/g" /etc/letsencrypt/options-ssl-apache.conf
 sed -i "s/:AES256-SHA256:/:/g" /etc/letsencrypt/options-ssl-apache.conf
 sed -i "s/:AES128-SHA:/:/g" /etc/letsencrypt/options-ssl-apache.conf
 sed -i "s/:AES256-SHA:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:DHE-RSA-AES128-GCM-SHA256:/:/g" /etc/letsencrypt/options-ssl-apache.conf
+sed -i "s/:DHE-RSA-AES256-GCM-SHA384:/:/g" /etc/letsencrypt/options-ssl-apache.conf
 service apache2 restart
